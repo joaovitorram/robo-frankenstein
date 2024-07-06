14eb543f2b014fa8ca4bbffc80fcaa1dcbc3f8d2
labinfo1 <labinfo1@ectufrn.ect.ufrn.br>
2024-07-06 12:21:24 -0300

    agora vai

--- codigo-robo-frankenstein/codigo-robo-frankenstein.ino
+++ codigo-robo-frankenstein/codigo-robo-frankenstein.ino
@@ -1,9 +1,27 @@
+int pinoM1a = 5;
+int pinoM1b = 6;
+int pinoM2a = 10;
+int pinoM2b = 11;
+
+
 void setup() {
-  // put your setup code here, to run once:
+  //motor 1
+pinMode(pinoM1a, OUTPUT);
+pinMode(pinoM1b, OUTPUT);
+// motor 2
+pinMode(pinoM2a, OUTPUT);
+pinMode(pinoM2b, OUTPUT);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(pinoM1a, HIGH );
+  digitalWrite(pinoM1b,LOW);
+
+
+  digitalWrite(pinoM2a, LOW );
+  digitalWrite(pinoM2b, HIGH);
+
 
 }
