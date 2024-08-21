db9c7f7d2d5a3526f3b60276ad9206454ce1d20b
joaovitorram <167652978+joaovitorram@users.noreply.github.com>
2024-08-20 22:51:58 -0300

    Update codigoYoutube.cpp

--- robo ura obr/codigoYoutube.cpp
+++ robo ura obr/codigoYoutube.cpp
@@ -1,53 +1,50 @@
-//Sensores
-#define pinSensorD 11
+// Sensores
+#define pinSensorD 13
 #define pinSensorE 12
 
-//Motores
-#define pinMotorD1 3
-#define pinMotoD2 5
-#define pinMotorE3 6
-#define pinMotorE4 9
+// Motores
+#define pinMotorD1 10
+#define pinMotorD2 9
+#define pinMotorE3 5
+#define pinMotorE4 6
 
 void setup() {
-//Sensores
-pinMode(pinSensorD, INPUT);
-pinMode(pinSensorE, INPUT);
+  // Sensores
+  pinMode(pinSensorD, INPUT);
+  pinMode(pinSensorE, INPUT);
 
-//Motores
-pinMode(pinMotorD1, OUTPUT);
-pinMode(pinMotorD2, OUTPUT);
-pinMode(pinMotorE3, OUTPUT);
-pinMode(pinMotorE4, OUTPUT);
-
+  // Motores
+  pinMode(pinMotorD1, OUTPUT);
+  pinMode(pinMotorD2, OUTPUT);
+  pinMode(pinMotorE3, OUTPUT);
+  pinMode(pinMotorE4, OUTPUT);
 }
 
 void loop() {
+  // Leitura dos sensores
+  bool estadoD = digitalRead(pinSensorD);
+  bool estadoE = digitalRead(pinSensorE);
 
-    bool estadoD = !digitalRead(pinSensorD);
-    bool estadoE = !digitalRead(pinSensorE);
-
-//Andar
-if(estadoD && estadoE) {
-    analogWrite(pinMotorD1, 90);
-    analogWrite(pinMotorE3, 90);
-}
-
-//Parar
-if(!estadoD && !estadoE)
+  // Andar
+  if (estadoD && estadoE) {
+    analogWrite(pinMotorD1, 110);
+    analogWrite(pinMotorE3, 110);
+  } 
+  // Parar
+  else if (!estadoD && !estadoE) {
     digitalWrite(pinMotorD1, LOW);
     digitalWrite(pinMotorD2, LOW);
-    digitalWrite(pinMotorE2, LOW);
-    digitalWrite(pinMotorE4, LOW);
-}
-
-//Ajustar a Direita
-if(!estadoD && estadoE) {
-    analogWrite(pinMotorD1, 90);
     digitalWrite(pinMotorE3, LOW);
-}
-
-//Ajustar a Esquerda
-if(estadoD && !estadoE) {
+    digitalWrite(pinMotorE4, LOW);
+  } 
+  // Ajustar a Direita
+  else if (estadoD && !estadoE) {
+    analogWrite(pinMotorD1, 115);
+    digitalWrite(pinMotorE3, LOW);
+  } 
+  // Ajustar a Esquerda
+  else if (!estadoD && estadoE) {
     digitalWrite(pinMotorD1, LOW);
-    analogWrite(pinMotorE3, 90);
+    analogWrite(pinMotorE3, 120);
+  }
 }
